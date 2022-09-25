4721142db0afe7a03e9c289953217d68a8201191
nong <11585934+nong-git@user.noreply.gitee.com>
2022-09-25 19:43:12 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <Home/>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <v-container>
+    <v-row>
+      <v-col
+        
+        v-for="n in 6"
+        :key="n"
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-6"
+          outlined
+          tile
+          color="blue"
+        >
+          肯定还是
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-bottom-navigation :value="value" color="teal" grow>
+    <v-btn>
+      <v-icon>mdi-heart</v-icon>
+      <span>主页</span>
+    </v-btn>
+    <v-btn>
+      <v-icon>mdi-map-marker</v-icon>
+      <span>分类</span>
+    </v-btn>
+    <v-btn>
+      <v-icon>mdi-map-marker</v-icon>
+      <span>更多</span>
+    </v-btn>
+    <v-btn>
+      <v-icon>mdi-history</v-icon>
+      <span>我的</span>
+    </v-btn>
+  </v-bottom-navigation>
+  
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
